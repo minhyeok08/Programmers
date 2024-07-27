928d8307fe510e20a7afdccb4e01c5e7cd325137
minhyeok08 <129153138+minhyeok08@users.noreply.github.com>
2024-07-27 23:58:38 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+ 
+        for (int i = 0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int start = command[0] - 1;
+            int end = command[1] - 1;
+            int k = command[2] - 1;
+ 
+            int[] temp = new int[end - start + 1];
+            int index = 0;
+            for (int j = start; j <= end; j++) {
+                temp[index++] = array[j];
+            }
+
+            Arrays.sort(temp);
+ 
+            answer[i] = temp[k];
+        }
+ 
+        return answer;
+    }
+}
+ 
